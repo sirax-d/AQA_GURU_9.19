e361ba6f97d04372b2871822baf2d15590e2bc1d
Krichanov Sergey <dsirax@icloud.com>
2024-01-27 16:03:28 +0100

    Refactor, + fix imports, README

--- test_api.py
+++ test_api.py
@@ -1,7 +1,6 @@
-import json
 import requests
-from schemas import *
 from jsonschema import validate
+from schemas import *
 
 url = 'https://reqres.in/api/users/'
 name_user = 'Test'
@@ -9,11 +8,13 @@ job_user = 'QA'
 email = 'eve.holt@reqres.in'
 password = 'pistol'
 
+
 def test_get_users():
     response = requests.get(url, params={'page': 2}, verify=False)
     body = response.json()
     validate(body, schema_users_list)
     assert response.status_code == 200
+
 
 def test_update_user():
     data_user = {'name': name_user, 'job': job_user}
@@ -23,10 +24,12 @@ def test_update_user():
     assert body['job'] == job_user
     assert body['updatedAt'] != ''
 
+
 def test_delete_user():
     response = requests.delete(url + '2', verify=False)
     assert response.text == ''
     assert response.status_code == 204
+
 
 def test_return_valid_name_positive():
     data_user = {'name': name_user, 'job': job_user}
@@ -35,6 +38,7 @@ def test_return_valid_name_positive():
     assert body['name'] == name_user
     assert body['job'] == job_user
     assert response.status_code == 201
+
 
 def test_return_valid_name_negative():
     data_user = {'name': name_user + 'negative', 'job': job_user + 'negative'}
@@ -49,10 +53,12 @@ def test_return_404():
     response = requests.get(url + '23234', verify=False)
     assert response.status_code == 404
 
+
 def test_return_400():
     new_url = url.replace(("/users/"), "/register/")
     response = requests.post(new_url, verify=False)
     assert response.status_code == 400
+
 
 def test_create_user_validate():
     data_user = {'name': name_user, 'job': job_user}
@@ -68,6 +74,7 @@ def test_register_schema_validate():
     response = requests.post(new_url, data=data_user, verify=False)
     body = response.json()
     validate(body, schema_register)
+
 
 def test_delayed_schema_validate():
     response = requests.get(url, params='3', verify=False)
